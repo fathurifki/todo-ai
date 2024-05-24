18e6f02ef740c1ca8aa744fdc96bc664a936c6fe
fathurifki <rifkifathu@gmail.com>
2024-05-24 12:54:27 +0700

    config tailwind css and grouping the routes

---
+++ src/todo/landing/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate a simple context to pass to the template rendering engine.
+    context = {
+        'title': 'Home Page',
+        'heading': 'Welcome to the Todo Application',
+    }
+    
+    # Render the HTML template index.html with the data in the context variable.
+    return render(request, 'landing/index.html', context=context)
